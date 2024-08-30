10b7900184d0b00663c5f4152f54f6726d5d40bf
miguel-hv <huertadelavegamiguel@gmail.com>
2024-08-30 11:54:23 +0200

    FEAT: initial commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using pokelab_api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConnectionDatabase");
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Auth pokelab",
+        Version = "v1"
+    });
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Please enter a token",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },[]
+        }
+    });
+
+});
+
+var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# pokelab-back
+
+Backend para el proyecto Angular pokelab. Estoy probando .net con ayuda de [este tutorial](https://www.youtube.com/watch?v=Gua0O0Q7I58&list=LL&index=27&t=2087s)
+usando el paquete Entity Framework Core como ORM
+
+## Notas
+- DbContext es una clase parte del Entity Framework. Representa una sesión de trabajo con la db para hacer CRUD mediante operaciones de dominio
+- Se instala el paquete Entity Framework Core SQL Server
+- Entity Framework Tools es el paquete para ejecutar las migraciones a la db
+- Para ejecutar las migraciones se hace desde la consola de paquetes Nuget
